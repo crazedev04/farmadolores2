3ba47b19fd2e46faea9f5d0d909b91b3375d3d31
cracken <logiudicenicolas04@gmail.com>
2026-04-07 23:07:07 -0300

    chore: add react-native-siren to transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-community|@react-navigation|react-native-gesture-handler|react-native-reanimated|react-native-safe-area-context|@react-native-async-storage|@notifee|@react-native-firebase)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-community|@react-navigation|react-native-gesture-handler|react-native-reanimated|react-native-safe-area-context|@react-native-async-storage|@notifee|@react-native-firebase|react-native-siren|sp-react-native-in-app-updates)/)',
   ],
   moduleNameMapper: {
     '\\.(ttf|otf|png|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
